e9baa0b759cb764c90781a6316dad285fcbf3aab
HeyNeek <45445231+HeyNeek@users.noreply.github.com>
2021-10-31 11:45:37 -0500

    added some basic rendering for sandwich data in Menu

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { Switch, Route } from "react-router-dom";
 import NavBar from './Navbar';
 import Home from "./Home";
@@ -7,16 +7,32 @@ import Create from "./Create"
 
 
 function App() {
-  const [pageState, setPageState] = useState("/")
+  //setting up state for page and sandwich data
+  const [pageState, setPageState] = useState("/");
+  const [sandwiches, setSandwiches] = useState([]);
 
+  //bringing in the sandwich data for use
+  useEffect(() => {
+    fetch("http://localhost:9292/sandwiches")
+      .then((r) => r.json())
+      .then((sandwiches) => setSandwiches(sandwiches));
+  }, []);
 
+  console.log(sandwiches);
+
+  //creating a new js variable to hold only the sandwich name string from the sandwich data
+  const displayedSandwiches = sandwiches.map(sandwich => sandwich.name);
+
+  console.log(displayedSandwiches);
+
+  //passing in displayedSandwiches into our Menu component for rendering
   return (
     <div className="App">
       <header>wassup playa</header>
       <NavBar onChangePage={setPageState} />
         <Switch>
           <Route path="/menu">
-            <Menu />
+            <Menu sandwiches={displayedSandwiches} />
           </Route>
           <Route path="/create">
             <Create />
--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-function Menu(){
+function Menu({sandwiches}){
+    //just rendering some of our sandwich data in a basic way, may need to add a seperate "Sandwich" component later for CSS purposes
     return (
         <div>
             <h1>Menu</h1>
+            <ul>
+                {sandwiches.map(sandwich => <li>{sandwich}</li>)}
+            </ul>
         </div>
     )
 }
